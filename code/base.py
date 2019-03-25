d1cc700d052a706cfb27d26182fb57186718c47d
Shivam Thapliyal <thapliyalshivam@gmail.com>
2019-03-26 00:35:46 +0530

    updation

--- code/base.py
+++ code/base.py
@@ -2,8 +2,8 @@ import os, sys, inspect, thread
 src_dir = os.path.dirname(inspect.getfile(inspect.currentframe()))
 lib_dir = os.path.abspath(os.path.join(src_dir, '../lib'))
 sys.path.insert(0, lib_dir)
-import Leap
-
+import cv2, Leap, math, ctypes
+import numpy as np
 ################################################################################
 # Copyright (C) 2012-2013 Leap Motion, Inc. All rights reserved.               #
 # Leap Motion proprietary and confidential. Not for distribution.              #
@@ -19,6 +19,7 @@ class SampleListener(Leap.Listener):
     finger_names = ['Thumb', 'Index', 'Middle', 'Ring', 'Pinky']
     bone_names = ['Metacarpal', 'Proximal', 'Intermediate', 'Distal']
     state_names = ['STATE_INVALID', 'STATE_START', 'STATE_UPDATE', 'STATE_END']
+    maps_initialized = False
 
     def on_init(self, controller):
         print "Initialized"
@@ -39,9 +40,84 @@ class SampleListener(Leap.Listener):
     def on_exit(self, controller):
         print "Exited"
 
+
+    def convert_distortion_maps(self,image):
+
+        distortion_length = image.distortion_width * image.distortion_height
+        xmap = np.zeros(distortion_length/2, dtype=np.float32)
+        ymap = np.zeros(distortion_length/2, dtype=np.float32)
+
+        for i in range(0, distortion_length, 2):
+            xmap[distortion_length/2 - i/2 - 1] = image.distortion[i] * image.width
+            ymap[distortion_length/2 - i/2 - 1] = image.distortion[i + 1] * image.height
+
+            xmap = np.reshape(xmap, (image.distortion_height, image.distortion_width/2))
+            ymap = np.reshape(ymap, (image.distortion_height, image.distortion_width/2))
+
+    #resize the distortion map to equal desired destination image size
+        resized_xmap = cv2.resize(xmap,
+                              (image.width, image.height),
+                              0, 0,
+                              cv2.INTER_LINEAR)
+        resized_ymap = cv2.resize(ymap,
+                              (image.width, image.height),
+                              0, 0,
+                              cv2.INTER_LINEAR)
+
+    #Use faster fixed point maps
+        coordinate_map, interpolation_coefficients = cv2.convertMaps(resized_xmap,
+                                                                 resized_ymap,
+                                                                 cv2.CV_32FC1,
+                                                                 nninterpolation = False)
+
+        return coordinate_map, interpolation_coefficients
+
+
+    def undistort(self, image, coordinate_map, coefficient_map, width, height):
+        destination = np.empty((width, height), dtype = np.ubyte)
+
+    #wrap image data in numpy array
+        i_address = int(image.data_pointer)
+        ctype_array_def = ctypes.c_ubyte * image.height * image.width
+    # as ctypes array
+        as_ctype_array = ctype_array_def.from_address(i_address)
+    # as numpy array
+        as_numpy_array = np.ctypeslib.as_array(as_ctype_array)
+        img = np.reshape(as_numpy_array, (image.height, image.width))
+
+    #remap image to destination
+        destination = cv2.remap(img,
+                            coordinate_map,
+                            coefficient_map,
+                            interpolation = cv2.INTER_LINEAR)
+
+    #resize output to desired destination size
+        destination = cv2.resize(destination,
+                             (width, height),
+                             0, 0,
+                             cv2.INTER_LINEAR)
+        return destination
     def on_frame(self, controller):
+
+
         # Get the most recent frame and report some basic information
         frame = controller.frame()
+        image = frame.images[0]
+        print(image.is_valid)
+        if image.is_valid:
+            if not self.maps_initialized:
+                left_coordinates, left_coefficients = self.convert_distortion_maps(frame.images[0])
+                right_coordinates, right_coefficients = self.convert_distortion_maps(frame.images[1])
+                maps_initialized = True
+
+
+            undistorted_left = self.undistort(image, left_coordinates, left_coefficients, 400, 400)
+            undistorted_right = self.undistort(image, right_coordinates, right_coefficients, 400, 400)
+
+            cv2.imshow('Left Camera', undistorted_left)
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                self.remove_listener(listener)
 
         print "Frame id: %d, timestamp: %d, hands: %d, fingers: %d, tools: %d, gestures: %d" % (
               frame.id, frame.timestamp, len(frame.hands), len(frame.fingers), len(frame.tools), len(frame.gestures()))
@@ -154,7 +230,7 @@ def main():
     # Create a sample listener and controller
     listener = SampleListener()
     controller = Leap.Controller()
-
+    controller.set_policy_flags(Leap.Controller.POLICY_IMAGES)
     # Have the sample listener receive events from the controller
     controller.add_listener(listener)
 
